c61810a4e13b7383ccad42d68e3679b368c8aecd
yammmit <151710591+yammmit@users.noreply.github.com>
2024-11-18 01:23:43 +0900

    docs : 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services")
-    id("kotlin-android")
-    //id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") // 추가된 플러그인
     kotlin("kapt") // Kapt 플러그인 추가
 }
 
-// local.properties 사용을 위함
 val properties = Properties().apply {
     load(rootProject.file("local.properties").inputStream())
 }
@@ -27,14 +24,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        // url 변수 지정
+        // BuildConfig 변수 정의
         buildConfigField("String", "URL_WEATHER", "\"${properties["url.weather"]}\"")
-
-        // api key 변수 지정
         buildConfigField("String", "API_KEY", "\"${properties["api.key"]}\"")
-
-        // kakao map rest api key 변수 지정
-        //buildConfigField("String", "KAKAO_MAP_REST_API_KEY", properties["kakao.map.rest.api.key"] as String)
     }
 
     buildTypes {
@@ -46,6 +38,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -53,7 +46,6 @@ android {
 
     kotlinOptions {
         jvmTarget = "1.8"
-        freeCompilerArgs = listOf("-Xuse-experimental=kotlin.Experimental")
     }
 
     buildFeatures {
@@ -63,28 +55,31 @@ android {
 }
 
 dependencies {
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.core.ktx)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.database)
+    implementation ("androidx.appcompat:appcompat:1.6.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
+    implementation("com.google.firebase:firebase-auth")
 
-    // retrofit2
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation("com.squareup.retrofit2:converter-simplexml:2.11.0") // 추가된 XML 파싱
+    // Retrofit2 & Gson
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-simplexml:2.11.0")
 
-    // 위치
+    // 위치 서비스
     implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("com.google.code.gson:gson:2.11.0")
 
-    //애니메이션 아이콘
-    implementation ("com.airbnb.android:lottie:6.6.0")
+    // 애니메이션 아이콘
+    implementation("com.airbnb.android:lottie:6.0.0")
 }
